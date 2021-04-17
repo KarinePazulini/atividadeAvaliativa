2453b3f4fc38ff98acda8190e12be381c24ebfa7
karinePazu <>
2021-04-17 02:58:20 +0000

    Mudança de variável e ajuste para receber a palavra

--- main.cpp
+++ main.cpp
@@ -3,24 +3,27 @@
 
 using namespace std;
 
+string palavra;
+
 int main() {
+  cout << "Digite a palavra com no máximo 50 caracteres: \n" ;
+  cin >> palavra;
 
-string palavra = "karine";
-int x; int y; int c; int l;
+  int tecla; int qtde; int linha; int coluna;
 
-char letras [4][8] = {{'a','d','g','j','m','p','t','w'}, {'b','e','h','k','n','q','u','x'}, {'c','f','i','l','o','r','v','y'}, {' ',' ',' ',' ',' ','s',' ','z'}};
+  char letras [4][8] = {{'a','d','g','j','m','p','t','w'}, {'b','e','h','k','n','q','u','x'}, {'c','f','i','l','o','r','v','y'}, {' ',' ',' ',' ',' ','s',' ','z'}};
 
-for(int p=0; p<size(palavra);p++)
-{
-  if(palavra[p]==' '){
+ for(int p=0;p<size(palavra);p++)
+  {
+  if(palavra[p]== ' '){
   cout << "Entrada inválida, tente novamente!" << endl;
   break;
   }else{
-    for(int c=0;c<8;c++){
-      for(int l=0; l<4;l++){
-        if(palavra[p] == letras[l][c]){
-          x=c+2; y=l+1;
-        cout << "#" << x << "="<< y << endl;
+    for(int linha=0; linha<4;linha++){
+      for(int coluna=0;coluna<8;coluna++){
+        if(palavra[p] == letras[linha][coluna]){
+          tecla=coluna+2; qtde=linha+1;
+        cout << "#" << tecla << "="<< qtde << endl;
         }
       }
     }
